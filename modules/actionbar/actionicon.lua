0ef0742c24379bce071e318b50fe090ca065cc55
NaifuKishi <gaming@apelmail.ch>
2026-02-15 12:59:58 +0100

    removed accidently colored macro overlay

--- modules/actionbar/actionicon.lua
+++ modules/actionbar/actionicon.lua
@@ -362,7 +362,7 @@ function uiElements.actionIcon(name, parent, barIndex, buttonIndex)
 				macroFrame:SetPoint("CENTER", frame, "CENTER", 1, 1)
 				macroFrame:SetSecureMode("restricted")
 				macroFrame:SetMouseMasking("limited")
-				macroFrame:SetBackgroundColor(1, 0, 0, .2)
+				--macroFrame:SetBackgroundColor(1, 0, 0, .2)
 				
 				local thisSize = frame:GetWidth() -2
 				macroFrame:SetWidth(thisSize)
